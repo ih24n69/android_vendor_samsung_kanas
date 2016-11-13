a3e5d3862cad739f254d0ca97e3d228d6b9b4644
ih24n69 <muhammadihsan69@gmail.com>
2016-11-13 07:12:21 +0700

    Add vendor tree for CM12.1 base

--- kanas-vendor-blobs.mk
+++ kanas-vendor-blobs.mk
@@ -16,9 +16,18 @@
 
 PRODUCT_COPY_FILES += \
     vendor/samsung/kanas/proprietary/bin/at_distributor:system/bin/at_distributor \
-    vendor/samsung/kanas/proprietary/bin/npsmobex:system/bin/npsmobex \
-    vendor/samsung/kanas/proprietary/bin/refnotify:system/bin/refnotify \
     vendor/samsung/kanas/proprietary/bin/engpc:system/bin/engpc \
+    vendor/samsung/kanas/proprietary/bin/ext_data.sh:system/bin/ext_data.sh \
+    vendor/samsung/kanas/proprietary/bin/ext_kill.sh:system/bin/ext_kill.sh \
+    vendor/samsung/kanas/proprietary/bin/ext_symlink.sh:system/bin/ext_symlink.sh \
+    vendor/samsung/kanas/proprietary/bin/gps.cer:system/bin/gps.cer \
+    vendor/samsung/kanas/proprietary/bin/gpsd:system/bin/gpsd \
+    vendor/samsung/kanas/proprietary/bin/macloader:system/bin/macloader \
+    vendor/samsung/kanas/proprietary/bin/modemd:system/bin/modemd \
+    vendor/samsung/kanas/proprietary/bin/nvitemd:system/bin/nvitemd \
+    vendor/samsung/kanas/proprietary/bin/phoneserver:system/bin/phoneserver \
+    vendor/samsung/kanas/proprietary/bin/refnotify:system/bin/refnotify \
+    vendor/samsung/kanas/proprietary/bin/rild:system/bin/rild \
     vendor/samsung/kanas/proprietary/lib/libatparser.so:system/lib/libatparser.so \
     vendor/samsung/kanas/proprietary/lib/libsamsungearcare.so:system/lib/libsamsungearcare.so \
     vendor/samsung/kanas/proprietary/lib/libsamsungeffect.so:system/lib/libsamsungeffect.so \
@@ -39,21 +48,13 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/kanas/proprietary/etc/tiny_hw.xml:system/etc/tiny_hw.xml \
     vendor/samsung/kanas/proprietary/lib/libfactoryutil.so:system/lib/libfactoryutil.so \
     vendor/samsung/kanas/proprietary/lib/libomission_avoidance.so:system/lib/libomission_avoidance.so \
+    vendor/samsung/kanas/proprietary/lib/libatchannel.so:system/lib/libatchannel.so \
     vendor/samsung/kanas/proprietary/etc/wifi/bcmdhd_apsta.bin:system/etc/wifi/bcmdhd_apsta.bin \
     vendor/samsung/kanas/proprietary/etc/wifi/bcmdhd_sta.bin:system/etc/wifi/bcmdhd_sta.bin \
     vendor/samsung/kanas/proprietary/etc/wifi/bcmdhd_mfg.bin:system/etc/wifi/bcmdhd_mfg.bin \
     vendor/samsung/kanas/proprietary/etc/wifi/nvram_net.txt:system/etc/wifi/nvram_net.txt \
     vendor/samsung/kanas/proprietary/etc/wifi/nvram_mfg.txt:system/etc/wifi/nvram_mfg.txt \
-    vendor/samsung/kanas/proprietary/bin/macloader:system/bin/macloader \
     vendor/samsung/kanas/proprietary/vendor/firmware/BCM4330B1_002.001.003.1025.1303.hcd:system/vendor/firmware/BCM4330B1_002.001.003.1025.1303.hcd \
-    vendor/samsung/kanas/proprietary/bin/rild:system/bin/rild \
-    vendor/samsung/kanas/proprietary/bin/modemd:system/bin/modemd \
-    vendor/samsung/kanas/proprietary/bin/nvitemd:system/bin/nvitemd \
-    vendor/samsung/kanas/proprietary/bin/phoneserver:system/bin/phoneserver \
-    vendor/samsung/kanas/proprietary/bin/ext_data.sh:system/bin/ext_data.sh \
-    vendor/samsung/kanas/proprietary/bin/ext_kill.sh:system/bin/ext_kill.sh \
-    vendor/samsung/kanas/proprietary/bin/ext_symlink.sh:system/bin/ext_symlink.sh \
-    vendor/samsung/kanas/proprietary/bin/prepare_param.sh:system/bin/prepare_param.sh \
     vendor/samsung/kanas/proprietary/lib/libril.so:system/lib/libril.so \
     vendor/samsung/kanas/proprietary/lib/libreference-ril_sp.so:system/lib/libreference-ril_sp.so \
     vendor/samsung/kanas/proprietary/lib/librilutils.so:system/lib/librilutils.so \
@@ -66,12 +67,9 @@ PRODUCT_COPY_FILES += \
     vendor/samsung/kanas/proprietary/lib/hw/sensors.sc8830.so:system/lib/hw/sensors.sc8830.so \
     vendor/samsung/kanas/proprietary/lib/libwvm.so:system/lib/libwvm.so \
     vendor/samsung/kanas/proprietary/lib/libomafldrm.so:system/lib/libomafldrm.so \
-    vendor/samsung/kanas/proprietary/lib/drm/libdrmwvmplugin.so:system/lib/drm/libdrmwvmplugin.so \
     vendor/samsung/kanas/proprietary/lib/drm/libomaplugin.so:system/lib/drm/libomaplugin.so \
     vendor/samsung/kanas/proprietary/vendor/lib/libwvdrm_L3.so:system/vendor/lib/libwvdrm_L3.so \
     vendor/samsung/kanas/proprietary/vendor/lib/libWVStreamControlAPI_L3.so:system/vendor/lib/libWVStreamControlAPI_L3.so \
-    vendor/samsung/kanas/proprietary/vendor/lib/mediadrm/libwvdrmengine.so:system/vendor/lib/mediadrm/libwvdrmengine.so \
-    vendor/samsung/kanas/proprietary/bin/lpm:system/bin/lpm \
     vendor/samsung/kanas/proprietary/lib/libQmageDecoder.so:system/lib/libQmageDecoder.so \
     vendor/samsung/kanas/proprietary/media/battery_charging_5.qmg:system/media/battery_charging_5.qmg \
     vendor/samsung/kanas/proprietary/media/battery_charging_10.qmg:system/media/battery_charging_10.qmg \
